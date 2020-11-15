c1d33794f08fab56df17da1ae552dff0b1fc9b08
Pontus Andersson <panderss@c1r4p1.hive.fi>
2020-11-15 22:55:54 +0200

    Viewing and deleting orders.

--- delete_order.php
+++ delete_order.php
@@ -1,20 +1,18 @@
-<!DOCTYPE html>
-<html>
-<head>
+<html><head>
     <meta charset="UTF-8">
-    <title>Order management</title>
+    <title>Order(s) deleted!</title>
     <link rel="stylesheet" href="rush.css">
 </head>
-<body>
-<ul>
+<body><ul>
 	<li><a class="active"  href="#droids">Droids</a></li>
 	<li><a href="#search">Search</a></li>
 	<li><a href="#cart">Cart</a></li>
 	<li style="float:right"><a href="#logout">LogOut</a></li>
-</ul>	
-<h1>Order Management</h1><br />
+</ul>
+<h1>Order(s) deleted!</h1><br />
 
-<form action="delete_user.php" method="POST">
+
+
 <?php
 session_start();
 
@@ -67,8 +65,6 @@ $conn = mysqli_connect('127.0.0.1:3306', $username, $password, "droids");
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-
-$implosion = implode(":", $_SESSION["basket"]);
 if (!mysqli_query($conn, $sql))
 {
   echo "Error: " . $sql . "<br>" . mysqli_error($conn);
@@ -79,51 +75,28 @@ if (!$conn)
 {
   die ("Connection failed: " . mysqli_connect_error());
 }
-$sql = "SELECT * FROM orders";
-$result = mysqli_query($conn, $sql);
-if (mysqli_num_rows($result) > 0)
-{
-	while($row = mysqli_fetch_assoc($result))
-	{
-		echo "Customer: " . $row["username"]. " - Contents: " . $row["contents"]. " <input type=\"checkbox\" id=\"selection\" name=\"del_order[]\" value=\"".$row["id"]."\"> <br /><br />";
-		/*$products = explode($row["contents"]);
-		$sql2="SELECT * FROM droids WHERE id IN (";
-		foreach($products as $id => $value) 
-		{ 
-			$sql2.=$id.",";
-		}
-		$sql2 = substr($sql2, 0, -1).") ORDER BY name ASC"; 
-		//echo "$sql<br />";
-		$result = mysqli_query($conn, $sql2);
-		//print_r ($result);
-		//echo "<br />";
-		if (mysqli_num_rows($result) > 0)
-		{
+$del_user = $_POST['del_order'];
+print_r($del_user);
 
-	  // output data of each row
-		  			//echo "Matchin row for $elem.<br />";
-					while($row2 = mysqli_fetch_assoc($result))
-					{
-						foreach ($basket as $elem)
-						{
-						//echo "Trying to match ".$row["id"]." with ".$elem."<br />";
-	//	echo "id: " . $row["id"]. " - Name: " . $row["name"]. " - Category: " . $row["cat"]. " - Price: " . $row["price"]. " credits\n";
-							if($row["id"] === $elem) /// here you can determine what you are searching for
-								echo "id: " . $row2["id"]. " - Name: " . $row2["name"]. " - Price: " . $row2["price"]. " credits <input type=\"checkbox\" id=\"selection\" name=\"basket_item[]\" value=\"".$row2["id"]."\"> <br />";
-						//break ;
-						}
-					}
-		}*/
+
+foreach($del_user as $elem) 
+{
+	$sql="DELETE FROM orders WHERE id = $elem";
+	echo $sql."<br />";
+	if (mysqli_query($conn, $sql))
+	{
+	echo "Record deleted successfully";
+	}
+	else
+	{
+		echo "Error deleting record: " . mysqli_error($conn);
 	}
 }
+
+
 mysqli_close($conn);
+
 ?>
 
-<input type="submit" value="Delete order">
 </form>
-<form action="delete_order.php" method="POST">
-
-</form>
-
 </body></html>
-
