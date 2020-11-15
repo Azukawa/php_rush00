b84fe11a0c0ba2de7cf3574d55453dfeaa7ecc59
Azukawa <aleksi.sukava@gmail.com>
2020-11-15 21:52:36 +0200

    now can shop form wellcome

--- welcome.php
+++ welcome.php
@@ -25,6 +25,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 	<li><a href="view_basket.php">Basket</a></li>
 	
 	<li style="float:right"><a href="logout.php">LogOut</a></li>
+
 	<li style="float:right"><a href="admin.php">Admin</a></li>
 </ul>
 <div>
@@ -45,59 +46,32 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         <a href="reset-password.php">Reset Your Password</a>
         <a href="logout.php">Sign Out of Your Account</a>
 	</p>
-	<span>Cart===>	 <a href="view_basket.php">View your cart</a>.</span>
-
+	<span>Cart===>	 <a href="view_basket.php">View your cart</a>.</span><br/><br/>
+<form action="add_to_basket.php" method="POST">
 <?php
 //////////////////////
 ////////////////////
-echo "XXXXX";
-print_r($row['name']);
-//echo "<h2>You searched for:</h2>Search terms: \"".$_POST['search_terms'] ."\" by: ". $_POST["search-by"]."</p><br />";
-
-//if ($_POST["submit"] === 'Search' && $_POST['search_terms'] && $_POST['search-by'])
-{
-	//$_POST['search terms'] = string searched for
-//$servername = "localhost"; useless crap.
-//echo "In search function if <br />";
 $username = "root";
 $password = "mampba";
-$dbname = "droids";
 
-// Create connection
-$conn = mysqli_connect('127.0.0.1:3306', $username, $password, $dbname);
 
+$conn = mysqli_connect('127.0.0.1:3306', $username, $password, "droids");
 if (!$conn)
 {
   die ("Connection failed: " . mysqli_connect_error());
 }
-$sql = "SELECT id, name, year, price FROM droids";
-//echo $sql."<br />";
+$sql = "SELECT * FROM droids";
 $result = mysqli_query($conn, $sql);
-//print_r ($result);
-//$opt = "cat";
-//$optarg = "Tall";
 if (mysqli_num_rows($result) > 0)
 {
-  // output data of each row
-  $i = 0;
 	while($row = mysqli_fetch_assoc($result))
-	{
-	print_r($result[$row]);
-//	echo "id: " . $row["id"]. " - Name: " . $row["name"]. " - Category: " . $row["cat"]. " - Price: " . $row["price"]. " credits\n";
-	//	if($row[$_POST['search-by']] === $_POST['search_terms']) /// here you can determine what you are searching for
-	//	echo "id: " . $row["id"]. " - Name: " . $row["name"]. " - Price: " . $row["price"]. " credits <input type=\"checkbox\" id=\"selection\" name=\"droid[]\" value=\"".$row["id"]."\"> <br />";
-	}
+		echo "id: " . $row["id"]. " - Name: " . $row["name"]. " - Price: " . $row["price"]. " credits <input type=\"checkbox\" id=\"selection\" name=\"droid[]\" value=\"".$row["id"]."\"> <br />";
 }
-else
-{
-  echo "0 results\n";
-}
-//print_r($result);
-mysqli_close($conn); // close connection.
-}
+mysqli_close($conn);
 //////////////////////////
 ////////////////////////
 ?>
+<input type="submit" value="Add to basket">
 <div class="row">
   <div class="column">
     <img src="droid_pics/BB-8.jpg" alt="BB-8" style="width:20%">
